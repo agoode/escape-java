86ea324db0d91d3e3a72125635f1f18e5b02c6f1
Adam Goode <adam@spicenitz.org>
2004-12-15 04:05:31 +0000

    yes!

---
+++ Escape/org/spacebar/escape/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Created on Dec 14, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.spacebar.escape;
+
+/**
+ * @author adam
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
